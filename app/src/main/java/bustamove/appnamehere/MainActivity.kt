b4740a6b2d9f10846bb68350f60ea4cd50c8325b
Hiren-Bhavsar <bhavsar.hsb@gmail.com>
2022-05-01 16:37:49 -0400

    Added required permissions functions

--- app/src/main/java/bustamove/appnamehere/MainActivity.kt
+++ app/src/main/java/bustamove/appnamehere/MainActivity.kt
@@ -1,19 +1,29 @@
 package bustamove.appnamehere
 
+import android.annotation.SuppressLint
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import androidx.appcompat.widget.Toolbar
 import androidx.viewpager.widget.ViewPager
 import bustamove.appnamehere.ui.HomeFragment
 import bustamove.appnamehere.ui.PlaylistFragment
 import bustamove.appnamehere.ui.SettingsFragment
 import bustamove.appnamehere.ui.ViewPagerAdapter
+import com.google.android.gms.location.ActivityRecognition
+import com.google.android.gms.location.ActivityRecognitionClient
 import com.google.android.material.tabs.TabLayout
+import pub.devrel.easypermissions.EasyPermissions
 
 private lateinit var pager: ViewPager
 private lateinit var tab: TabLayout
 
-class MainActivity : AppCompatActivity() {
+@SuppressLint("StaticFieldLeak")
+private lateinit var activityClient: ActivityRecognitionClient
+private lateinit var userPrefs : SharedPreferences
+
+class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +31,10 @@ class MainActivity : AppCompatActivity() {
 
         pager = findViewById(R.id.viewPager)
         tab = findViewById(R.id.tabs)
+        activityClient = ActivityRecognition.getClient(this)
+        userPrefs = PreferenceManager.getDefaultSharedPreferences(this)
+
+
 
         val adapter = ViewPagerAdapter(supportFragmentManager)
 
@@ -31,5 +45,13 @@ class MainActivity : AppCompatActivity() {
         pager.adapter = adapter
 
         tab.setupWithViewPager(pager)
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        TODO("Not yet implemented")
     }
 }
