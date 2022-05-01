5076f0ab44ef05adc1f8df33013c85d684c828f3
Hiren-Bhavsar <bhavsar.hsb@gmail.com>
2022-05-01 14:27:09 -0400

    MainActivity Cleanup

--- app/src/main/java/bustamove/appnamehere/MainActivity.kt
+++ app/src/main/java/bustamove/appnamehere/MainActivity.kt
@@ -14,6 +14,7 @@ private lateinit var pager: ViewPager
 private lateinit var tab: TabLayout
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
