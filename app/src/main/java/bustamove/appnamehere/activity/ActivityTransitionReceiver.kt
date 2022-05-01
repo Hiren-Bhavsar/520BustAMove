d22c6ef62e72a99efe1271df2bc1b3a5e2b7d934
Hiren-Bhavsar <bhavsar.hsb@gmail.com>
2022-05-01 16:13:02 -0400

    Update ActivityTransitionReceiver.kt

--- app/src/main/java/bustamove/appnamehere/activity/ActivityTransitionReceiver.kt
+++ app/src/main/java/bustamove/appnamehere/activity/ActivityTransitionReceiver.kt
@@ -1,4 +1,9 @@
 package bustamove.appnamehere.activity
 
+//Code based on android examples provided by Google
+//https://developer.android.com/guide/topics/location/transition
+
 class ActivityTransitionReceiver {
+
+
 }
