6ca5da88235639d184df26a1d0419b6a3eea05bb
Hiren-Bhavsar <bhavsar.hsb@gmail.com>
2022-05-01 13:59:19 -0400

    Tab page setup completed

--- app/src/main/java/bustamove/appnamehere/MainActivity.kt
+++ app/src/main/java/bustamove/appnamehere/MainActivity.kt
@@ -2,10 +2,33 @@ package bustamove.appnamehere
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.viewpager.widget.ViewPager
+import bustamove.appnamehere.ui.HomeFragment
+import bustamove.appnamehere.ui.PlaylistFragment
+import bustamove.appnamehere.ui.SettingsFragment
+import bustamove.appnamehere.ui.ViewPagerAdapter
+import com.google.android.material.tabs.TabLayout
+
+private lateinit var pager: ViewPager
+private lateinit var tab: TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        pager = findViewById(R.id.viewPager)
+        tab = findViewById(R.id.tabs)
+
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+
+        adapter.addFragment(PlaylistFragment(), "Playlist")
+        adapter.addFragment(HomeFragment(), "Home")
+        adapter.addFragment(SettingsFragment(), "Settings")
+
+        pager.adapter = adapter
+
+        tab.setupWithViewPager(pager)
     }
 }
---
+++ app/src/main/java/bustamove/appnamehere/ui/PlaylistFragment.kt
@@ -0,0 +1,17 @@
+package bustamove.appnamehere.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import bustamove.appnamehere.R
+
+class PlaylistFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) =
+        inflater.inflate(R.layout.fragment_playlist, container, false)!!
+}
