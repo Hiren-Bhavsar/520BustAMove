5df5adaa96102ec9f976aa8497d153ba4068c501
Hiren-Bhavsar <bhavsar.hsb@gmail.com>
2022-05-01 16:24:47 -0400

    Added getting activity info
    Still need to decide where to store it...

--- app/src/main/java/bustamove/appnamehere/activity/ActivityTransitionReceiver.kt
+++ app/src/main/java/bustamove/appnamehere/activity/ActivityTransitionReceiver.kt
@@ -1,9 +1,22 @@
 package bustamove.appnamehere.activity
 
+import android.content.Context
+import android.content.Intent
+import com.google.android.gms.location.ActivityTransitionResult
+
 //Code based on android examples provided by Google
 //https://developer.android.com/guide/topics/location/transition
 
 class ActivityTransitionReceiver {
 
-
+    fun onReceive(context: Context, intent: Intent) {
+        if (ActivityTransitionResult.hasResult(intent)) {
+            val activityResult = ActivityTransitionResult.extractResult(intent)!!
+            activityResult.let {
+                activityResult.transitionEvents.forEach{activityEvent ->
+                    val activityInfo = ActivityTransitionsUtility.toTransitionType(activityEvent.transitionType) + " " + ActivityTransitionsUtility.toActivityString(activityEvent.activityType);
+                }
+            }
+        }
+    }
 }
